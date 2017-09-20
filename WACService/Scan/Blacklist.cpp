05b668de57b428f5e909a7fcad89fb537d1c4bc4
Andrew <andrewdepke@gmail.com>
2017-09-20 11:06:17 -0600

    Added Basic Process/File Blacklist Scanner

---
+++ WACService/Scan/Blacklist.cpp
@@ -0,0 +1,33 @@
+#include "Blacklist.h"
+
+#include <Windows.h>
+
+const wchar_t* ProcessBlacklist[] =
+{
+	L"WinDbgFrameClass",
+	L"OLLYDBG",
+};
+
+const wchar_t* FileBlacklist[] =
+{
+	L"CEHYPERSCANSETTINGS",
+};
+
+bool BlacklistScan()
+{
+	for (auto Process : ProcessBlacklist)
+	{
+		if (FindWindow((LPCWSTR)Process, NULL))
+		{
+			return true;
+		}
+	}
+
+	for (auto File : FileBlacklist)
+	{
+		if (OpenFileMapping(FILE_MAP_READ, false, (LPCWSTR)File))
+		{
+			return true;
+		}
+	}
+}
